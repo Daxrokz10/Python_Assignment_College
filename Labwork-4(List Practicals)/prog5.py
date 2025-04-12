ede7e8f74075402b1ed0d108020e35004c157b4a
Daxrokz10 <dakshgagnani@gmail.com>
2025-04-12 13:37:01 +0530

    List practicals

---
+++ Labwork-4(List Practicals)/prog5.py
@@ -0,0 +1,8 @@
+# Create a list with mixed data types and access each type.
+
+list = [1, 2.5, "Hello", True, None]
+print("Integer:", list[0])
+print("Float:", list[1])
+print("String:", list[2])
+print("Boolean:", list[3])
+print("None:", list[4])
